c21988d3290aabf232a687d9136cca7d8130a05e
Malik Alharbi <malik.f.alharbi@gmail.com>
2023-11-28 17:47:32 +0300

    BE 2 - Exercise 4 (Events)

--- Exercises/app.js
+++ Exercises/app.js
@@ -13,3 +13,24 @@ fileSystem.readdir(fileName, (err, files) => {
   else console.log(files);
   process.exit();
 });
+
+
+const customEmitter = require('./eventEmitter');
+
+function simulateUserLogin() {
+  const randomDelay = Math.random() * (2 - 0.1) + 0.1;
+  const user = `USER_${Math.floor(Math.random() * 10) + 1}`;
+
+  setTimeout(() => {
+    const timestamp = new Date().toISOString();
+    console.log(`${timestamp}: ${user} logged in`);
+    customEmitter.emit('userLoggedIn', { timestamp, user });
+    simulateUserLogin();
+  }, randomDelay * 1000);
+}
+
+customEmitter.on('userLoggedIn', ({ timestamp, user }) => { });
+
+customEmitter.on('userLoggedOut', ({ timestamp, user }) => { });
+
+simulateUserLogin();
